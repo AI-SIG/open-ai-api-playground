4d68fce4ac900f3959e63744c7a5dd0154bfa445
De Villiers Neethling <devilliers.neethling@standardbank.co.za>
2023-04-11 19:21:42 +0200

    moving constants.py around

--- Speech to Image Converter/image_generator.py
+++ Speech to Image Converter/image_generator.py
@@ -1,4 +1,22 @@
+import openai
+import requests
+import os
+import constants
+# from PIL import Image
+
+# set environment key
+openai.api_key = constants.OPEN_AI_API_KEY
+
 class ImageGenerator:
     @staticmethod
     def generate_image_from_prompt(prompt):
+        image_dir_name = "images"
+        image_dir = os.path.join(os.curdir, image_dir_name)
+        if not os.path.isdir(image_dir):
+            os.mkdir(image_dir)
         
+        print(f"{image_dir=}")
+
+
+ImageGenerator.generate_image_from_prompt("")
+
