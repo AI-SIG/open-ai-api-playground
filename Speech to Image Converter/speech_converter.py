b91379d8c674a43a5d27d5cbe53e90b50c39601d
De Villiers Neethling <devilliers.neethling@standardbank.co.za>
2023-04-11 16:21:34 +0200

    Made SpeechConverter a class with a static method. Naughty, naughty but this isn't production code

--- Speech to Image Converter/speech_converter.py
+++ Speech to Image Converter/speech_converter.py
@@ -1,32 +1,38 @@
-# For speech recognition:
-#pip install SpeechRecognition
-# For accessing mic:
+# For speech recognition
+# pip install SpeechRecognition
+# For accessing mic
 # pip install PyAudio
 
 import speech_recognition as sr
 
-def get_prompt_from_speech():
-    """
-    Prompts a meat robot for a speech description and 
-    converts that speech into text
-    """
-    r = sr.Recognizer()
-    audio_text = ""
+class SpeechConverter:
 
-    # listening the speech and store in audio_text variable
-    with sr.Microphone() as source:
-        print("[Talk, hooman]")
-        audio = r.listen(source)
-        print("[Talking time over, thanks hooman]")
-        
-        # recoginize_google() method will throw a request error if the API is unreachable
-        try:
-            # using google speech recognition
-            audio_text = r.recognize_google(audio)
-        except:
-            print("[Sorry, I did not get that?!]")
-    print("Text:  "+ audio_text)
-    return audio_text
+    @staticmethod
+    def get_prompt_from_speech():
+        """
+        Prompts a meat robot for a speech description and 
+        converts that speech into text
+        """
+        r = sr.Recognizer()
+        audio_text = ""
 
-prompt = get_prompt_from_speech()
+        # listening the speech and store in audio_text variable
+        with sr.Microphone() as source:
+            print("[Talk, hooman]")
+            audio = r.listen(source)
+            print("[Talking time over, thanks hooman]")
+
+            # recognize_google() method will throw a request error if the API is unreachable
+            try:
+                # using google speech recognition
+                audio_text = r.recognize_google(audio)
+            except:
+                print("[Sorry, I did not get that?! Open ya mouf!]")
+        print("Text:  " + audio_text)
+        return audio_text
+
+
+
+# prompt = SpeechConverter.get_prompt_from_speech()
+# prompt = "The Golden Girls on crack"
 # print("Prompt: " + prompt)
