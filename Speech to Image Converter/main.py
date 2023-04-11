f0ad88bf43963ee1c6ec00bf9dbe4adffdcc6897
De Villiers Neethling <devilliers.neethling@standardbank.co.za>
2023-04-11 20:54:27 +0200

    Makeshift dependency management. Didn't test it. Please be the sucker to test it.

--- Speech to Image Converter/main.py
+++ Speech to Image Converter/main.py
@@ -1,6 +1,10 @@
 import speech_converter as sc
 import image_generator as ig
 
+# Install python-pip if it isn't installed
+# Use pip install -r requirements.txt to install all dependencies.
+# NB this has not been tested so cthulhuspeed
+
 def main():
     prompt = sc.SpeechConverter.get_prompt_from_speech()
     ig.ImageGenerator.generate_image_from_prompt(prompt)
