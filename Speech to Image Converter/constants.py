5d34f1120d65fa6a135918334ec2ce1b6df9069a
De Villiers Neethling <devilliers.neethling@standardbank.co.za>
2023-04-12 14:34:07 +0200

    More info for key usage

--- Speech to Image Converter/constants.py
+++ Speech to Image Converter/constants.py
@@ -1,5 +1,6 @@
 import private_keys
 
-# Replace private_keys.MY_OPEN_AI_API_KEY with your own open API key 
+# Replace private_keys.MY_OPEN_AI_API_KEY with your own open API key, 
+# eg OPEN_AI_API_KEY = "yourKeyHereKeepTheBunnyEars" 
 # Keys are available from https://platform.openai.com/account/api-keys
 OPEN_AI_API_KEY = private_keys.MY_OPEN_AI_API_KEY
